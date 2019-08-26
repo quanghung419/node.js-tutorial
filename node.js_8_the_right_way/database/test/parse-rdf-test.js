b72846da3014eae4a3f83401fde1ef5b47887821
HungDQ <quanghung419@gmail.com>
2019-08-26 09:35:56 +0700

    Processing data files sequentially

--- node.js_8_the_right_way/database/test/parse-rdf-test.js
+++ node.js_8_the_right_way/database/test/parse-rdf-test.js
@@ -1,7 +1,8 @@
 'use strict';
+
 const fs = require('fs');
 const expect = require('chai').expect;
-const parseRDF = require('../lib/parse-rdf');
+const parseRDF = require('../lib/parse-rdf.js');
 
 const rdf = fs.readFileSync(`${__dirname}/pg132.rdf`);
 
@@ -10,6 +11,21 @@ describe('parseRDF', () => {
         expect(parseRDF).to.be.a('function');
     });
 
-    const book = parseRDF(rdf);
-    expect(book).to.be.an('object');
+    it('should parse RDF content', () => {
+        const book = parseRDF(rdf);
+
+        expect(book).to.be.an('object');
+        expect(book).to.have.a.property('id', 132);
+        expect(book).to.have.a.property('title', 'The Art of War');
+
+        expect(book).to.have.a.property('authors')
+            .that.is.an('array').with.lengthOf(2)
+            .and.contains('Sunzi, active 6th century B.C.')
+            .and.contains('Giles, Lionel');
+
+        expect(book).to.have.a.property('subjects')
+            .that.is.an('array').with.lengthOf(2)
+            .and.contains('Military art and science -- Early works to 1800')
+            .and.contains('War -- Early works to 1800');
+    });
 });

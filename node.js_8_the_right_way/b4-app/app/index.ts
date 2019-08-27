29d0e260260ab8cced9e18d6e87315236bb67758
HungDQ <quanghung419@gmail.com>
2019-08-27 17:18:07 +0700

    Templating HTML with Handlebars

--- node.js_8_the_right_way/b4-app/app/index.ts
+++ node.js_8_the_right_way/b4-app/app/index.ts
@@ -2,8 +2,13 @@ import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap';
 import * as templates from './templates.ts';
 
-document.body.innerHTML = templates.main;
+// Page setup.
+document.body.innerHTML = templates.main();
 const mainElement = document.body.querySelector('.b4-main');
 const alertsElement = document.body.querySelector('.b4-alerts');
-mainElement.innerHTML = templates.welcome;
-alertsElement.innerHTML = templates.alert;
+
+mainElement.innerHTML = templates.welcome();
+alertsElement.innerHTML = templates.alert({
+    type: 'info',
+    message: 'Handlebars is working!'
+});

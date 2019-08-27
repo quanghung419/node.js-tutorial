e04ce18a33c47e7e8d5e6450626b570aca3912d7
HungDQ <quanghung419@gmail.com>
2019-08-27 16:54:02 +0700

    + Sprucing up your UI with Bootstrap + Bringing in Bootstrap JavaScript and jQuery

--- node.js_8_the_right_way/b4-app/webpack.config.js
+++ node.js_8_the_right_way/b4-app/webpack.config.js
@@ -2,6 +2,8 @@
 const path = require('path');
 const distDir = path.resolve(__dirname, 'dist');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const webpack = require('webpack');
+
 module.exports = {
     entry: './entry.js',
     output: {
@@ -15,6 +17,22 @@ module.exports = {
     plugins: [
         new HtmlWebpackPlugin({
             title: 'Better Book Bundle Builder'
+        }),
+        new webpack.ProvidePlugin({
+            $: 'jquery',
+            jQuery: 'jquery'
         })
-    ]
+    ],
+    module: {
+        rules: [
+            {
+                test: /\.css$/,
+                use: ['style-loader', 'css-loader']
+            },
+            {
+                test: /\.(png|woff|woff2|eot|ttf|svg)$/,
+                loader: 'url-loader?limit=100000'
+            }
+        ]
+    }
 };

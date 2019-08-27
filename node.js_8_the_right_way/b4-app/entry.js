eecf12b883b0fd5ca205db0b6f78367c52778d9e
HungDQ <quanghung419@gmail.com>
2019-08-27 16:41:28 +0700

    Generating your first webpack bundle

---
+++ node.js_8_the_right_way/b4-app/entry.js
@@ -0,0 +1,5 @@
+'use strict';
+document.body.innerHTML = `
+    <h1>B4 - Book Bundler</h1>
+    <p>${new Date()}</p>
+`;
--- node.js_8_the_right_way/b4-app/webpack.config.js
+++ node.js_8_the_right_way/b4-app/webpack.config.js
@@ -1,4 +1,20 @@
 'use strict';
+const path = require('path');
+const distDir = path.resolve(__dirname, 'dist');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 module.exports = {
-    entry: './entry.js'
+    entry: './entry.js',
+    output: {
+        filename: 'bundle.js',
+        path: distDir
+    },
+    devServer: {
+        contentBase: distDir,
+        port: 60800
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            title: 'Better Book Bundle Builder'
+        })
+    ]
 };
